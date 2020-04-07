3b6b260a29bf1b8f3be986acad995bd021f50254
Apostolis Karageorgopoulos <aposkarag@gmail.com>
2020-04-07 16:58:54 +0300

    Updates

---
+++ precache-manifest.3fa83a2df06737a457413241517c5325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2666e07f1cffb0d7c62797b7f8505b1",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "27a48e05f546adc67cb2",
+    "url": "/contactmanager/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "5bf5c42d50f0ef70603f",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "27a48e05f546adc67cb2",
+    "url": "/contactmanager/static/js/2.e523710c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.e523710c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf5c42d50f0ef70603f",
+    "url": "/contactmanager/static/js/main.93954001.chunk.js"
+  },
+  {
+    "revision": "2b43dc7d0ef5c78fcd0f",
+    "url": "/contactmanager/static/js/runtime-main.08b2bbf7.js"
+  }
+]);
